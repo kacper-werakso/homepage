08bd67ac4bd7676ac240418c2d3eeabc7d058e64
Kacper Werakso <kacperwerakso@o2.pl>
2022-07-18 13:13:43 +0200

    zmiany-2

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const firstName = 'Kacper';
 const age = '23'
 
+console.log(
+Witaj na stronie poświęconej Rezerwatu przyrody Żurawiniec, nazywam się ${firstName} i mam ${age}
+);
 
 const heading = document.querySelector('.main__heading--js');
 
